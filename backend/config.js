d1a446a622cbf5364daeda278d48c5f80896f524
Shivshankar <info.shivshankar2002@kgpian.iitkgp.ac.in>
2024-02-08 00:32:44 +0530

    first commit

---
+++ backend/config.js
@@ -0,0 +1,7 @@
+export const PORT = 5555;
+
+export const mongoDBURL =
+  'mongodb+srv://shivbookstore:shivBooks@bookstoremern.cyu0we3.mongodb.net/?retryWrites=true&w=majority';
+
+// Please create a free database for yourself.
+// This database will be deleted after tutorial
